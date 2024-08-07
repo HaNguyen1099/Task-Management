3ef6797d71f98bb6a97050c71791a3dfdd319261
Ha Nguyen Ngoc <nnha.1099@gmail.com>
2024-08-07 22:37:00 +0700

    Create project Task-Management

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express")
+const database = require("./config/database")
+require("dotenv").config()
+
+const app = express()
+const port = process.env.PORT
+
+database.connect()
+
+const Task = require("./models/task.model")
+
+app.get("/tasks", async (req, res) => {
+    const tasks = await Task.find({
+        deleted: false
+    })
+
+    // console.log(tasks)
+
+    res.json(tasks)
+})
+
+app.get("/tasks/detail/:id", async (req, res) => {
+    try {
+        const id = req.params.id 
+
+        const task = await Task.findOne({
+            _id: id,
+            deleted: false
+        })
+
+        // console.log(task)
+
+        res.json(task)
+
+    } catch (error) {
+        res.json("Không tìm thấy!")
+    }
+    
+})
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`)
+})
