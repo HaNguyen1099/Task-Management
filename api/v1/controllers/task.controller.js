b2e8a6ba6c2580de128538c6dbd7396e3ffcb04b
Ha Nguyen Ngoc <nnha.1099@gmail.com>
2024-08-08 21:02:46 +0700

    Tính năng sắp xếp theo tiêu chí

--- api/v1/controllers/task.controller.js
+++ api/v1/controllers/task.controller.js
@@ -6,11 +6,19 @@ module.exports.index = async (req, res) => {
         deleted: false
     }
 
+    // Filter Status 
     if (req.query.status) {
         find.status = req.query.status
     }
+   
+    // Sort 
+    const sort = {}
 
-    const tasks = await Task.find(find)
+    if (req.query.sortKey && req.query.sortValue) {
+        sort[req.query.sortKey] = req.query.sortValue
+    }
+
+    const tasks = await Task.find(find).sort(sort)
 
     res.json(tasks)
 }
