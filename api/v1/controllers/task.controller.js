9fe865f6183d3acea74d2b9d50b97236d5339ca7
Ha Nguyen Ngoc <nnha.1099@gmail.com>
2024-08-11 21:43:30 +0700

    Lấy ra danh sách theo đúng user

--- api/v1/controllers/task.controller.js
+++ api/v1/controllers/task.controller.js
@@ -2,10 +2,15 @@ const Task = require("../models/task.model")
 
 const paginationHelper = require("../../../helpers/pagination")
 const searchHelper = require("../../../helpers/search")
+const { list } = require("./user.controller")
 
 // [GET] /api/v1/tasks
 module.exports.index = async (req, res) => {
     const find = {
+        $or: [
+            {createdBy: req.user.id},
+            {listUser: req.user.id}
+        ],
         deleted: false
     }
 
